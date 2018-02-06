7195d0962510e0e4f4ab9a1c2e2e725d4e32523b
chillten <bogovich.alexander@gmail.com>
2018-02-06 10:04:50 +0700

    add logger

--- src/main/kotlin/com/bogovich/excel/ExcelWorkSheetHandler.kt
+++ src/main/kotlin/com/bogovich/excel/ExcelWorkSheetHandler.kt
@@ -1,4 +1,13 @@
 package com.bogovich.excel
 
+import com.bogovich.excel.callback.ExcelRowContentCallback
+import mu.KLogging
+
 class ExcelWorkSheetHandler {
+    companion object: KLogging()
+    val head_row = 0;
+    private var rowCallback: ExcelRowContentCallback? = null
+    private var currentRow = 0
+
+
 }
